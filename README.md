05922cc95c4a2a34fb8d5f5011c3bb2246062af8
Guido Cioni <guidocioni@gmail.com>
2020-09-20 17:25:32 +0200

    New update after a while. Refactored the code in pot_data.py. Remove old NCL script. Added option to plot data already subsetted to avoid memory usage. See README

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # snow_ims
 Shows how to download and process IMS Snow Cover data with Python. 
 
-The main script is `download_plot_data.run`, which calls the Python script `plot_data.py`. 
+![](http://guidocioni.altervista.org/snow/archive/ims_nh_example.png)
 
-Additional script are provided with different map projections `map_projection_ims.py` and tools to read the IMS data directly from the URL without having to download anything (`ims_reader.py`). Note that one needs to download the coordinate files beforehand since reading directly from the server would require too much time. 
+The main script is `download_plot_data.run`, which calls the Python script `plot_data.py` where the data is read and some plots produced on different projections. These are defined for convenience in `map_projection_ims.py`
 
-Also note that 1-km data were so big that they needed to be sub-setted before plotting them. 
+The main utilities to read the data are contained in `ims_reader.py`. Please note the following. 
+- Coordinates for 4 km resolution data (`imslon_4km.bin` and `imslat_4km.bin`) are already downloaded and placed in the same folder to avoid having to read from the (slow) server every time 
+- The 4 km daily data is read directly from the server using pandas as it is not too big: we can avoid having to download and process it beforehand. 
+- Coordinates for the 1 km resolution data are already downloaded and saved as `pickle` for the same reason. Furthermore they are already subsetted on the Eurasian continent (see image below) as the original files are too big to be saved (8 GB in total!) ![Subset area](http://guidocioni.altervista.org/snow/archive/ims_subset.png)
+- The 1 km daily data is downloaded and processed BEFORE launching the Python script in `download_plot_data.run` so as to avoid having to load it into Python, thus saving memory and CPU. We do in a quite clever way using only `sed` and `cut`: `sed -ne '9031,17030p' ims_file.asc | cut -c11001-19000 > ims_file_subsetted_1km.asc`. If you find an easier way...do let me know! 
+
--- plot_data.py
+++ plot_data.py
@@ -2,43 +2,67 @@ import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 import pandas as pd
-from mpl_toolkits.basemap import Basemap # Import the Basemap toolkit
+from mpl_toolkits.basemap import Basemap  # Import the Basemap toolkit
 import matplotlib.pyplot as plt
-import datetime
+from datetime import datetime
 import ims_reader
 import map_projections_ims
+from matplotlib.offsetbox import AnchoredText
+import os
+
+base_folder = str(os.getenv['WORK_FOLDER'])
+coordinate_folder = base_folder
+DPI = 150
+# Define Color Map with discrete colors 
+cmap = matplotlib.colors.ListedColormap(["#144682", "#648214", "#82aac8", "#f0f0f0"])
+bounds = [0, 1, 2, 3, 4]
+norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
+
+year = datetime.now().year
+doy = datetime.now().strftime('%j')
+
+def annotation(ax, text, loc='upper right',fontsize=8):
+    """Put a general annotation in the plot."""
+    at = AnchoredText('%s'% text, prop=dict(size=fontsize), frameon=True, loc=loc)
+    at.patch.set_boxstyle("round,pad=0.,rounding_size=0.1")
+    at.zorder = 10
+    ax.add_artist(at)
+    return(at)
 
 def save_figure(folder, area, lon_to_plot, lat_to_plot, data_to_plot):
-	bmap=map_projections_ims.get_projection(area)
-	x,y=bmap(lon_to_plot, lat_to_plot)
-	bmap.contourf(x, y, data_to_plot, levels=np.array([0,1,2,3,4,5]),\
- 			 colors=('dodgerblue', 'seagreen','plum','white'), extend='min')
-	cbar=bmap.colorbar(location='right')
-	cbar.set_ticklabels(['Ocean','Land','Ice','Snow'])
-	cbar.ax.set_xticklabels(['Water','Land','Ice','Snow'])
-	plt.title(datetime.datetime.strptime(str(year)+str(doy),"%Y%j").strftime("%Y-%m-%d")+' - IMS 1km data - elaborated by www.guidocioni.it')
+    fig = plt.figure(figsize=(15, 15))
 
-	DPI = 150
-	plt.savefig(folder+"ims_"+area+"_"+datetime.datetime.strptime(str(year)+str(doy),"%Y%j").strftime("%Y%m%d")+".png"\
-		, dpi=DPI, bbox_inches='tight')
-	plt.close()
+    bmap = map_projections_ims.get_projection(area)
+    x, y = bmap(lon_to_plot, lat_to_plot)
 
-year=datetime.datetime.now().timetuple().tm_year
-#doy=datetime.datetime.now().timetuple().tm_yday
-doy=datetime.datetime.now().strftime('%j')
+    bmap.contourf(x, y, data_to_plot, levels = bounds, cmap = cmap)
 
-base_folder='/scratch/local1/m300382/snow/'
-coordinate_folder='/home/mpim/m300382/snow_ims/coordinates/'
+    # we don't really need a colorbar taking parts of the screen 
+    # ax2 = fig.add_axes([0.3, 0.18, 0.4, 0.03])
+    # cb2 = matplotlib.colorbar.ColorbarBase(ax2, cmap=cmap,
+    #                                 norm=norm,
+    #                                 boundaries=bounds,
+    #                                 ticks=[0.5, 1.5, 2.5, 3.5, 4.5],
+    #                                 spacing='proportional',
+    #                                 orientation='horizontal')
+    # cb2.set_ticklabels(['Ocean', 'Land', 'Ice', 'Snow'])
+    # cb2.ax.tick_params(size=0)
 
-lon_4km=ims_reader.read_lons_4km(path=coordinate_folder)
-lat_4km=ims_reader.read_lats_4km(path=coordinate_folder)
-data_4km=ims_reader.read_data_4km(year=year, doy=doy)
+    annotation(plt.gca(), 'Data of '+datetime.now().strftime("%Y-%m-%d"), 'upper right', '10')
+    annotation(plt.gca(), 'IMS 1km data - NSIDC. Snow=white, Sea ice=light blue', 'upper left', '10')
+    annotation(plt.gca(), 'Elaborated by Guido Cioni', 'lower right', '10')
+    
+    plt.savefig(folder + "ims_" + area + "_" + datetime.now().strftime("%Y%m%d") + ".png", dpi=DPI, bbox_inches='tight')
+    plt.close()
 
-save_figure(base_folder, 'nh', lon_4km, lat_4km, data_4km)
+lon_4km = ims_reader.read_lons_4km(path=coordinate_folder)
+lat_4km = ims_reader.read_lats_4km(path=coordinate_folder)
+data_4km = ims_reader.read_data_4km(year=year, doy=doy)
 
-lon_1km=ims_reader.read_lons_1km(path=coordinate_folder)
-lat_1km=ims_reader.read_lats_1km(path=coordinate_folder)
-data_1km=ims_reader.read_data_1km(year=year, doy=doy)
+save_figure(base_folder, 'alps', lon_4km, lat_4km, data_4km)
+
+lon_1km, lat_1km = ims_reader.read_coordinates_1km_compressed(path=coordinate_folder)
+data_1km = ims_reader.read_data_1km_compressed(base_folder+'ims_%s_%s_1km.asc' % (year, doy))
 
 save_figure(base_folder, 'eurasia', lon_1km, lat_1km, data_1km)
 save_figure(base_folder, 'italy', lon_1km, lat_1km, data_1km)
